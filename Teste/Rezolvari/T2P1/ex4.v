e9f84e5d000e3f172db6d69d66d1199f0cb6c1fc
Daria-1303 <daria.ciobanu@student.upt.ro>
2024-12-08 23:56:28 +0200

    idk

--- Teste/Rezolvari/T2P1/ex4.v
+++ Teste/Rezolvari/T2P1/ex4.v
@@ -20,6 +20,68 @@ module ex4(
     output [7 : 0] P
 );
 
+wire [3 : 0] suma [0 : 2];
+wire [3 : 0] carry_out;
 
+assign P[0] = X[0] & Y[0];
+
+rca_4bit rca_4bit_instanta1(
+    .nr1({Y[1] & X[3], Y[1] & X[2], Y[1] & X[1], Y[1] & X[0]}),
+    .nr2({1'b0, Y[0] & X[3], Y[0] & X[2], Y[0] & X[1]}),
+    .carry_in(1'b0),
+    .suma(suma[0]),
+    .carry_out(carry_out[0])
+);
+
+assign P[1] = suma[0][0];
+
+rca_4bit rca_4bit_instanta2(
+    .nr1({Y[2] & X[3], Y[2] & X[2], Y[2] & X[1], Y[2] & X[0]}),
+    .nr2({carry_out[0], suma[0][3], suma[0][2], suma[0][1]}),
+    .carry_in(1'b0),
+    .suma(suma[1]),
+    .carry_out(carry_out[1])
+);
+
+assign P[2] = suma[1][0];
+
+rca_4bit rca_4bit_instanta3(
+    .nr1({Y[3] & X[3], Y[3] & X[2], Y[3] & X[1], Y[3] & X[0]}),
+    .nr2({carry_out[1], suma[1][3], suma[1][2], suma[1][1]}),
+    .carry_in(1'b0),
+    .suma(suma[2]),
+    .carry_out(carry_out[2])
+);
+
+assign P[6 : 3] = suma[2][3 : 0];
+assign P[7] = carry_out[2];
+
+endmodule
+
+module ex4_tb();
+
+reg [3 : 0] X;
+reg [3 : 0] Y;
+
+wire [7 : 0] P;
+
+ex4 ex4_instanta(
+    .X(X),
+    .Y(Y),
+    .P(P)
+);
+
+
+initial begin
+    $display("X Y P");
+    $monitor("%d %d %d", X, Y, P);
+
+    repeat(16) begin
+        X = $urandom;
+        Y = $urandom;
+
+        #10;
+    end
+end
 
 endmodule
