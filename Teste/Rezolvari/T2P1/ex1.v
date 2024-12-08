58484ab25e4c03ef3a2d47d8bde5ba9e8ae62038
Daria-1303 <daria.ciobanu@student.upt.ro>
2024-12-08 21:38:07 +0200

    finished ex1 t2p1

--- Teste/Rezolvari/T2P1/ex1.v
+++ Teste/Rezolvari/T2P1/ex1.v
@@ -3,23 +3,23 @@ module ex1(
     input b,
     input clk,
     input rst_b,
-    output m,
-    output n
+    output reg m,
+    output reg n
 );
 
 // Stari -> 5 -> 3 biti
 // dar vrem codificare one cold
-localparam S0_ST  =  5'b01111;
-localparam S1_ST  =  3'b10111;
-localparam S2_ST  =  3'b11011;
-localparam S3_ST  =  3'b11101;
-localparam S4_ST  =  3'b11110;
+localparam S0_ST  =  5'b01111;  // 15
+localparam S1_ST  =  5'b10111;  // 23
+localparam S2_ST  =  5'b11011;  // 27
+localparam S3_ST  =  5'b11101;  // 29
+localparam S4_ST  =  5'b11110;  // 30
 
 reg [4:0] state;
 reg [4:0] next_state;
 
 // facem logica de stare + iesire
-always(*) begin
+always @(*) begin
     case(state)
         S0_ST : begin
             if(~a) begin
@@ -57,7 +57,7 @@ always(*) begin
         end
         S3_ST : begin 
             if(a & ~b) begin
-                next_sate = S3_ST;
+                next_state = S3_ST;
                 m = 1'b0;
                 n = 1'b0;
             end
@@ -100,5 +100,63 @@ always @(posedge clk, negedge rst_b) begin
         state <= next_state;
 end
 
+
+endmodule
+
+module ex1_tb;
+
+reg clk;
+reg rst_b;
+reg a;
+reg b;
+wire m;
+wire n;
+
+ex1 ex1_inst(
+    .a(a),
+    .b(b),
+    .clk(clk),
+    .rst_b(rst_b),
+    .m(m),
+    .n(n)
+);
+
+    localparam CLK_PERIOD=100; 
+    localparam CLK_CYCLES=7;
+    initial begin
+        clk = 0;
+        repeat (2*CLK_CYCLES) #(CLK_PERIOD/2) clk = ~clk;
+    end
+
+    localparam RST_PULSE=25;
+    initial begin
+        rst_b = 0;
+        #(RST_PULSE) rst_b = 1;
+    end
+
+    initial begin
+        $display("clk | rst | a | b | m | n | stare");
+        $monitor("%b | %b | %b | %b | %b | %b | %d", clk, rst_b, a, b, m, n, ex1_inst.state);
+        // pornim din starea 0 si vrem sa ajungem in starea 0
+        a = 0;
+
+        //vrem sa ajungem in starea starea 4
+        #(CLK_PERIOD)   a = 1; b = 1;
+
+        //vrem sa ajungem in starea 1
+        #(CLK_PERIOD)   b = 1;
+
+        // oricum ajungem in starea 2
+        #(CLK_PERIOD)   a = 1;
+
+        // ajungem in starea 3
+        #(CLK_PERIOD)   a = 1;
+
+        // ajungem in starea 0
+        #(CLK_PERIOD)   a = 0; b = 0;
+
+
+
+    end
 
 endmodule
